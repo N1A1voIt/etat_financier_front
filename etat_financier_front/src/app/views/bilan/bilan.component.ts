b6bb4ade0e03dd282864905148a2538a6ccdf8c0
N1A1voIt <120253454+N1A1voIt@users.noreply.github.com>
2025-01-03 14:17:47 +0300

    Indicateur financier

--- etat_financier_front/src/app/views/bilan/bilan.component.ts
+++ etat_financier_front/src/app/views/bilan/bilan.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import {MiniTabBilanComponent} from "../../atoms/mini-tab-bilan/mini-tab-bilan.component";
+import {
+    MatAccordion,
+    MatExpansionPanel,
+    MatExpansionPanelDescription, MatExpansionPanelHeader,
+    MatExpansionPanelTitle
+} from "@angular/material/expansion";
+import {CollapsibleComponent} from "../../atoms/collapsible/collapsible.component";
 
 @Component({
   selector: 'app-bilan',
   standalone: true,
-  imports: [
-    MiniTabBilanComponent
-  ],
+    imports: [
+        MiniTabBilanComponent,
+        MatExpansionPanel,
+        MatAccordion,
+        MatExpansionPanelTitle,
+        MatExpansionPanelDescription,
+        MatExpansionPanelTitle,
+        MatExpansionPanelHeader,
+        CollapsibleComponent
+    ],
   templateUrl: './bilan.component.html',
   styleUrl: './bilan.component.css'
 })
