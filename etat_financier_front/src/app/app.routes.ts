bcb2346f46f904058d3edf9a10d47a110d9cd7a4
N1A1voIt <120253454+N1A1voIt@users.noreply.github.com>
2025-01-03 12:15:03 +0300

    Bilan front

--- etat_financier_front/src/app/app.routes.ts
+++ etat_financier_front/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { IndexComponent } from './views/index/index.component';
 import { SigninrespComponent } from './views/signinresp/signinresp.component';
 import { HomeComponent } from './views/home/home.component';
 import {ResultComponent} from "./views/result/result.component";
+import {BilanComponent} from "./views/bilan/bilan.component";
 
 export const routes: Routes = [
     {path:'signup',component:IndexComponent},
     {path: 'signin',component:IndexComponent},
     {path: 'signinresp',component:SigninrespComponent},
     {path: 'result',component:ResultComponent},
+    {path: 'bilan',component:BilanComponent},
     // {path: 'home',component:HomeComponent},
     // {path:'bon-commande-',component:BonCommandeClientComponent,canActivate:[pasSousFifreGuardGuard]}
 ];
