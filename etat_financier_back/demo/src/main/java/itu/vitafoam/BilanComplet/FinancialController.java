7b0d84ae5b9280b46968e4d258c84deec3881ea0
N1A1voIt <120253454+N1A1voIt@users.noreply.github.com>
2025-01-04 18:18:00 +0300

    Last commit

--- etat_financier_back/demo/src/main/java/itu/vitafoam/BilanComplet/FinancialController.java
+++ etat_financier_back/demo/src/main/java/itu/vitafoam/BilanComplet/FinancialController.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -63,28 +64,45 @@ public class FinancialController {
         Long idBilan = b.get(0).getIdBilan();
 
         List<FinancialItemDTO> capitauxPropresDTO = ((List<CapitauxPropres>) capitauxPropresRepository.findByBilan(idBilan)).stream()
-                .map(c -> new FinancialItemDTO("Capital Social", c.getCapitalSocial()))
-                .collect(Collectors.toList());
+                .flatMap(c -> Stream.of(
+                        new FinancialItemDTO("Capital Social", c.getCapitalSocial()),
+                        new FinancialItemDTO("Reserves", c.getReserves()),
+                        new FinancialItemDTO("Resultat Exercice", c.getResultatExercice())
+                )).collect(Collectors.toList());
         bilan.put("CapitauxPropres", capitauxPropresDTO);
 
         List<FinancialItemDTO> passifDTO = ((List<Passif>) passifRepository.findByBilan(idBilan)).stream()
-                .map(p -> new FinancialItemDTO("Dettes Fournisseurs", p.getDettesFournisseurs()))
-                .collect(Collectors.toList());
+                .flatMap(p -> Stream.of(
+                        new FinancialItemDTO("Dettes Fournisseurs", p.getDettesFournisseurs()),
+                        new FinancialItemDTO("Dettes Fiscales", p.getDettesFiscales()),
+                        new FinancialItemDTO("Autres Passifs Courants", p.getAutresPassifsCourants())
+                )).collect(Collectors.toList());
         bilan.put("Passif", passifDTO);
 
         List<FinancialItemDTO> passifNonCourantDTO = ((List<PassifNonCourant>) passifNonCourantRepository.findByBilan(idBilan)).stream()
-                .map(pn -> new FinancialItemDTO("Emprunts", pn.getEmprunts()))
-                .collect(Collectors.toList());
+                .flatMap(pn -> Stream.of(
+                        new FinancialItemDTO("Emprunts", pn.getEmprunts()),
+                        new FinancialItemDTO("Provisions", pn.getProvisions()),
+                        new FinancialItemDTO("Autres Passifs Non Courants", pn.getAutresPassifsNonCourants())
+                )).collect(Collectors.toList());
         bilan.put("PassifNonCourant", passifNonCourantDTO);
 
         List<FinancialItemDTO> actifCourantDTO = ((List<ActifCourant>) actifCourantRepository.findByBilan(idBilan)).stream()
-                .map(a -> new FinancialItemDTO("Stocks", a.getStocks()))
-                .collect(Collectors.toList());
+                .flatMap(a -> Stream.of(
+                        new FinancialItemDTO("Stocks", a.getStocks()),
+                        new FinancialItemDTO("Créances Clients", a.getCreancesClients()),
+                        new FinancialItemDTO("Disponibilités", a.getDisponibilites()),
+                        new FinancialItemDTO("Autres Actifs Courants", a.getAutresActifsCourants())
+                )).collect(Collectors.toList());
         bilan.put("ActifCourant", actifCourantDTO);
 
         List<FinancialItemDTO> actifImmobiliseDTO = ((List<ActifImmobilise>) actifImmobiliseRepository.findByBilan(idBilan)).stream()
-                .map(a -> new FinancialItemDTO("Immobilisations Corporelles", a.getImmobilisationsCorporelles()))
-                .collect(Collectors.toList());
+                .flatMap(a -> Stream.of(
+                        new FinancialItemDTO("Immobilisations Corporelles", a.getImmobilisationsCorporelles()),
+                        new FinancialItemDTO("Immobilisations Incorporelles", a.getImmobilisationsIncorporelles()),
+                        new FinancialItemDTO("Immobilisations Financières", a.getImmobilisationsFinancieres()),
+                        new FinancialItemDTO("Autres Immobilisations", a.getAutresImmobilisations())
+                )).collect(Collectors.toList());
         bilan.put("ActifImmobilise", actifImmobiliseDTO);
 
         return bilan;
