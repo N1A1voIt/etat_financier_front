58c60f201e1b0df0aae463d6fe33bb205eb36530
N1A1voIt <120253454+N1A1voIt@users.noreply.github.com>
2025-01-04 09:23:00 +0300

    Ai interpretations

--- etat_financier_back/demo/src/main/java/itu/vitafoam/indicateur/InterpretationsService.java
+++ etat_financier_back/demo/src/main/java/itu/vitafoam/indicateur/InterpretationsService.java
@@ -1,6 +1,10 @@
 package itu.vitafoam.indicateur;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import itu.vitafoam.YearCorpDTO;
+import itu.vitafoam.aiCall.AiService;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +16,9 @@ import java.util.List;
 public class InterpretationsService {
     @Autowired
     EntityManager entityManager;
-    public InterpretationsAndSolutions interpret(YearCorpDTO yearCorpDTO){
+    @Autowired
+    AiService aiService;
+    public InterpretationsAndSolutions interpret(YearCorpDTO yearCorpDTO) throws JsonProcessingException {
         Interpretations interpretations;
         Query query = entityManager.createNativeQuery("SELECT  " +
                 "    b.id_entreprise, " +
@@ -28,11 +34,19 @@ public class InterpretationsService {
                 "JOIN  " +
                 "    compte_resultat cr ON b.id_entreprise = cr.id_entreprise AND b.id_annee = cr.id_annee " +
                 "JOIN  " +
-                "    actif_courant ac ON b.id_bilan = ac.id_bilan WHERE b.id_entreprise=:idEntreprise AND b.id_annee=:idAnnee");
+                "    actif_courant ac ON b.id_bilan = ac.id_bilan WHERE b.id_entreprise=:idEntreprise AND b.id_annee=:idAnnee", Interpretations.class);
         query.setParameter("idEntreprise",yearCorpDTO.getIdEntreprise());
         query.setParameter("idAnnee",yearCorpDTO.getIdAnnee());
         List<Interpretations> interpretationsList = query.getResultList();
         interpretations = interpretationsList.get(0);
-        return null;
+        String jsonString = aiService.queryChatbot(interpretations.toString());
+        jsonString = jsonString.replace("```json","");
+        jsonString = jsonString.replace("```","");
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<RatioIntSol> ratios = objectMapper.readValue(jsonString, new TypeReference<List<RatioIntSol>>() {});
+        InterpretationsAndSolutions interpretationsAndSolutions = new InterpretationsAndSolutions();
+        interpretationsAndSolutions.setRatioIntSols(ratios);
+        return interpretationsAndSolutions;
     }
+
 }
