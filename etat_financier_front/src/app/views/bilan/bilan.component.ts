35a4c2f729a1407feb24ecf550719ffebf16d0a6
N1A1voIt <120253454+N1A1voIt@users.noreply.github.com>
2025-01-04 09:42:42 +0300

    Disposition alter

--- etat_financier_front/src/app/views/bilan/bilan.component.ts
+++ etat_financier_front/src/app/views/bilan/bilan.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MiniTabBilanComponent} from "../../atoms/mini-tab-bilan/mini-tab-bilan.component";
 import {CollapsibleComponent} from "../../atoms/collapsible/collapsible.component";
 import {
@@ -7,6 +7,11 @@ import {
     MatExpansionPanelDescription, MatExpansionPanelHeader,
     MatExpansionPanelTitle
 } from "@angular/material/expansion";
+import {PaginatorModule} from "primeng/paginator";
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {SelectRoundedComponent} from "../../atoms/select-rounded/select-rounded.component";
+import {SubmitButtonComponent} from "../../../reusable/submit-button/submit-button.component";
+import {FormDataFinanceService} from "../form-data-finance/form-data-finance-service.service";
 @Component({
   selector: 'app-bilan',
   standalone: true,
@@ -18,11 +23,40 @@ import {
         MatExpansionPanelDescription,
         MatExpansionPanelTitle,
         MatExpansionPanelHeader,
-        CollapsibleComponent
+        CollapsibleComponent,
+        PaginatorModule,
+        ReactiveFormsModule,
+        SelectRoundedComponent,
+        SubmitButtonComponent
     ],
   templateUrl: './bilan.component.html',
   styleUrl: './bilan.component.css'
 })
-export class BilanComponent {
+export class BilanComponent implements OnInit{
+    formGroup : FormGroup;
+    data:any;
+    entreprises:any;
+    annees:any;
+    constructor(public formBuilder: FormBuilder,private formService:FormDataFinanceService) {
+        this.formGroup = formBuilder.group({
+            idEntreprise:[""],
+            idAnnee:[""]
+        })
+    }
+    onSubmit(){
 
+    }
+
+    ngOnInit(): void {
+        this.data = this.formService.enterpriseYear().subscribe({
+            next:(response)=>{
+                this.data = response;
+                this.entreprises = this.data.entreprises;
+                this.annees = this.data.annees;
+                console.log(this.data.entreprises);
+            },error:(error)=>{
+                alert(error);
+            }
+        })
+    }
 }
